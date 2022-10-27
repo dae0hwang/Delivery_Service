f683d8accb684cccae96e12c0f794f8bcfc393b4
hwang <geungan9@gmail.com>
2022-10-27 22:00:24 +0900

    Add transaction in the join service logic

--- APIDeliverySerivice/src/main/java/com/example/apideliveryservice/repository/CompanyMemberRepository.java
+++ APIDeliverySerivice/src/main/java/com/example/apideliveryservice/repository/CompanyMemberRepository.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CompanyMemberRepository {
 
-    private Connection connectJdbc() throws SQLException {
+    public Connection connectJdbc() throws SQLException {
         String server = "localhost:3307";
         String database = "delivery_service";
         String user_name = "root";
@@ -27,12 +27,11 @@ public class CompanyMemberRepository {
             "jdbc:mysql://" + server + "/" + database + "?useSSL=false", user_name, password);
     }
 
-    public void save(CompanyMemberDto companyMember) throws SQLException {
+    public void save(Connection connection, CompanyMemberDto companyMember) throws SQLException {
         String sql = "INSERT INTO company_members "
             + "(login_name, password, name, phone_verification, registration_date)"
             + "VALUES(?,?,?,?,?)";
-        try (Connection connection = connectJdbc();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, companyMember.getLoginName());
             preparedStatement.setString(2, companyMember.getPassword());
             preparedStatement.setString(3, companyMember.getName());
@@ -47,10 +46,9 @@ public class CompanyMemberRepository {
         }
     }
 
-    public Optional<CompanyMemberDto> findByName(String findName) {
+    public Optional<CompanyMemberDto> findByName(Connection connection, String findName) {
         String sql = "SELECT * FROM company_members WHERE name=?";
-        try (Connection connection = connectJdbc();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, findName);
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 if (resultSet.next()) {
@@ -64,10 +62,9 @@ public class CompanyMemberRepository {
         return Optional.empty();
     }
 
-    public Optional<CompanyMemberDto> findByLoginName(String loginName) {
+    public Optional<CompanyMemberDto> findByLoginName(Connection connection, String loginName) {
         String sql = "SELECT * FROM company_members WHERE login_name=?";
-        try (Connection connection = connectJdbc();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, loginName);
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 if (resultSet.next()) {
@@ -89,7 +86,7 @@ public class CompanyMemberRepository {
         int phoneVerification = resultSet.getInt(5);
         LocalDateTime createAt = LocalDate.parse(resultSet.getString(6)
             , DateTimeFormatter.ISO_DATE).atStartOfDay();
-        return new CompanyMemberDto(id,loginName, password, name, phoneVerification
+        return new CompanyMemberDto(id, loginName, password, name, phoneVerification
             , createAt);
     }
 }
--- APIDeliverySerivice/src/main/java/com/example/apideliveryservice/service/CompanyJoinService.java
+++ APIDeliverySerivice/src/main/java/com/example/apideliveryservice/service/CompanyJoinService.java
@@ -2,6 +2,7 @@ package com.example.apideliveryservice.service;
 
 import com.example.apideliveryservice.dto.CompanyMemberDto;
 import com.example.apideliveryservice.repository.CompanyMemberRepository;
+import java.sql.Connection;
 import java.sql.SQLException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,16 +14,31 @@ public class CompanyJoinService {
     private final CompanyMemberRepository companyMemberRepository;
 
     public void join(CompanyMemberDto companyMemberDto) throws SQLException {
-        validateDuplicateLoginNameOrName(companyMemberDto);
-        companyMemberRepository.save(companyMemberDto);
+        Connection connection = companyMemberRepository.connectJdbc();
+        try {
+            connection.setAutoCommit(false);
+            validateDuplicateLoginNameOrName(connection, companyMemberDto);
+            companyMemberRepository.save(connection, companyMemberDto);
+            connection.commit();
+        } catch (DuplicatedNameException e) {
+            connection.rollback();
+            throw new DuplicatedNameException();
+        } catch (DuplicatedLoginNameException e) {
+            throw new DuplicatedLoginNameException();
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
     }
 
-    private void validateDuplicateLoginNameOrName(CompanyMemberDto companyMemberDto) {
-        companyMemberRepository.findByLoginName(companyMemberDto.getLoginName())
+    private void validateDuplicateLoginNameOrName(Connection connection
+        , CompanyMemberDto companyMemberDto) {
+        companyMemberRepository.findByLoginName(connection, companyMemberDto.getLoginName())
             .ifPresent(m -> {
                 throw new DuplicatedLoginNameException();
             });
-        companyMemberRepository.findByName(companyMemberDto.getName())
+        companyMemberRepository.findByName(connection, companyMemberDto.getName())
             .ifPresent(m -> {
                 throw new DuplicatedNameException();
             });
