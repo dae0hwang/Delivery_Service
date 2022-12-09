021145f507c96330e7b80e97dcc890960b2969a8
hwang <geungan9@gmail.com>
2022-12-09 23:45:10 +0900

    Criteria 적용실패 - 질문용 브랜치 org.hibernate.query.criteria.internal.BasicPathUsageException: Cannot join to attribute of basic type

--- APIDeliveryService/src/main/java/com/example/apideliveryservice/repository/OrderRepository.java
+++ APIDeliveryService/src/main/java/com/example/apideliveryservice/repository/OrderRepository.java
@@ -1,12 +1,18 @@
 package com.example.apideliveryservice.repository;
 
 import com.example.apideliveryservice.dto.GeneralMemberOrderDto;
+import com.example.apideliveryservice.entity.CompanyFoodEntity;
 import com.example.apideliveryservice.entity.OrderDetailEntity;
 import com.example.apideliveryservice.entity.OrderEntity;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -34,14 +40,51 @@ public class OrderRepository {
     }
 
     public List<GeneralMemberOrderDto> findOrderListByGeneralId(EntityManager em, Long generalId) {
-        String jpql = "select new com.example.apideliveryservice.dto.GeneralMemberOrderDto(o.registrationDate, o.id, o.generalId, od.foodId, cp.name, od.foodPrice, od.foodAmount, od.companyId) "
-            + "from OrderEntity as o \n"
-            + "join OrderDetailEntity as od on o=od.orderEntity\n"
-            + "join CompanyFoodEntity as cp on od.foodId=cp.id\n"
-            + "where o.generalId=:generalId\n"
-            + "order by o.registrationDate desc";
-        List<GeneralMemberOrderDto> findList = em.createQuery(jpql, GeneralMemberOrderDto.class)
-            .setParameter("generalId", generalId).getResultList();
-        return findList;
+//        String jpql =
+//            "select new com.example.apideliveryservice.dto.GeneralMemberOrderDto(o"
+//                + ".registrationDate, o.id, o.generalId, od.foodId, cp.name, od.foodPrice, od"
+//                + ".foodAmount, od.companyId) "
+//                + "from OrderEntity as o \n"
+//                + "join OrderDetailEntity as od on o=od.orderEntity\n"
+//                + "join CompanyFoodEntity as cp on od.foodId=cp.id\n"
+//                + "where o.generalId=:generalId\n" + "order by o.registrationDate desc";
+//
+//        List<GeneralMemberOrderDto> findList = em.createQuery(jpql, GeneralMemberOrderDto.class)
+//            .setParameter("generalId", generalId).getResultList();
+//        return findList;
+
+        CriteriaBuilder criteriaBuilder  = em.getCriteriaBuilder();
+        CriteriaQuery<GeneralMemberOrderDto> criteriaQuery = criteriaBuilder.createQuery(
+            GeneralMemberOrderDto.class);
+
+        Root<OrderDetailEntity> od = criteriaQuery.from(OrderDetailEntity.class);
+        Join<OrderDetailEntity, OrderEntity> o = od.join("orderEntity",
+            JoinType.INNER);
+        Join<OrderDetailEntity, CompanyFoodEntity> cp = od.join("foodId", JoinType.INNER);
+
+        criteriaQuery.select(criteriaBuilder.construct(GeneralMemberOrderDto.class, o.get("registrationDate"), o.get("id"), o.get("generalId"), od.get("foodId"), cp.get("name"), od.get("foodPrice"),
+                od.get("foodAmount"), od.get("companyId")))
+        .multiselect(od, o, cp)
+            .where(criteriaBuilder.equal(od.get("generaId"), generalId));
+
+        List<GeneralMemberOrderDto> resultList = em.createQuery(criteriaQuery).getResultList();
+        return resultList;
+    }
+
+    public List<GeneralMemberOrderDto> findOrderListByGeneralId2(EntityManager em, Long generalId) {
+        CriteriaBuilder criteriaBuilder  = em.getCriteriaBuilder();
+        CriteriaQuery<GeneralMemberOrderDto> criteriaQuery = criteriaBuilder.createQuery(
+            GeneralMemberOrderDto.class);
+        
+        Root<OrderDetailEntity> root = criteriaQuery.from(OrderDetailEntity.class);
+        Join<OrderDetailEntity, OrderEntity> joinOrderEntity = root.join("orderEntity",
+            JoinType.INNER);
+        Join<OrderDetailEntity, CompanyFoodEntity> joinFoodId = root.join("foodId", JoinType.INNER);
+
+        criteriaQuery.multiselect(root, joinOrderEntity, joinFoodId)
+            .where(criteriaBuilder.equal(root.get("generaId"), generalId));
+
+        List<GeneralMemberOrderDto> resultList = em.createQuery(criteriaQuery).getResultList();
+        return resultList;
     }
 }
