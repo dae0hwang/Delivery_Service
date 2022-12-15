f3e9058de3f06882c30045a6380ed86dad38546f
hwang <geungan9@gmail.com>
2022-12-15 23:29:03 +0900

    datasource get connection

--- APIDeliveryService/src/main/java/com/example/apideliveryservice/controller/OrderStatisticController.java
+++ APIDeliveryService/src/main/java/com/example/apideliveryservice/controller/OrderStatisticController.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
