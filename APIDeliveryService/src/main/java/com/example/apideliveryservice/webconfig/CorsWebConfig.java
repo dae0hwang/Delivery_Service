02286d05bab5a45049aabb40e3be7a8e6c38fb17
hwang <geungan9@gmail.com>
2022-12-20 09:27:25 +0900

    pofile별 CORS 적용하기

--- APIDeliveryService/src/main/java/com/example/apideliveryservice/webconfig/CorsWebConfig.java
+++ APIDeliveryService/src/main/java/com/example/apideliveryservice/webconfig/CorsWebConfig.java
@@ -1,5 +1,6 @@
 package com.example.apideliveryservice.webconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,9 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsWebConfig implements WebMvcConfigurer {
 
+    @Value("${url.ui:@null}")
+    private String uiUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:8080", "http://192.168.100.1:8081");
+            .allowedOrigins(uiUrl);
     }
 }
