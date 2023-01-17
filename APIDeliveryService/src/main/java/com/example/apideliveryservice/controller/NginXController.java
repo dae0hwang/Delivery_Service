101a27b986b67d0a3a01c2bf3c5ca362357ea1fa
geung <geungan9@gmail.com>
2023-01-17 15:16:21 +0900

    nginX

--- APIDeliveryService/src/main/java/com/example/apideliveryservice/controller/NginXController.java
+++ APIDeliveryService/src/main/java/com/example/apideliveryservice/controller/NginXController.java
@@ -1,6 +1,7 @@
 package com.example.apideliveryservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,17 +12,20 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Slf4j
 public class NginXController {
 
 
     @GetMapping("/a")
     public String first(HttpServletRequest request) {
+        log.info("들어옴");
         String url = request.getRequestURL().toString();
         return "apiA " + url;
     }
 
     @GetMapping("/a/b")
     public String second(HttpServletRequest request) {
+        log.info("들어옴");
         String url = request.getRequestURL().toString();
         return "apiB "+url;
     }
--- UIDeliveryService/src/main/java/com/example/uideliveryservice/controller/NginXController.java
+++ UIDeliveryService/src/main/java/com/example/uideliveryservice/controller/NginXController.java
@@ -1,25 +1,27 @@
 package com.example.uideliveryservice.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class NginXController {
 
     @GetMapping("/a")
     public String first(HttpServletRequest request) {
+        log.info("들어옴");
         String url = request.getRequestURL().toString();
         return "uiA " + url;
     }
 
     @GetMapping("/a/b")
     public String second(HttpServletRequest request) {
+        log.info("들어옴");
         String url = request.getRequestURL().toString();
         return "uiB "+url;
     }
