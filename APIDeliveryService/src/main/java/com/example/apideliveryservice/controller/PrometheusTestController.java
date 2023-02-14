bf94222b5ce9c08feb11e9141cb4b80a7c8e8b6a
geung <geungan9@gmail.com>
2023-02-14 09:54:12 +0900

    elk test setting

--- APIDeliveryService/src/main/java/com/example/apideliveryservice/controller/PrometheusTestController.java
+++ APIDeliveryService/src/main/java/com/example/apideliveryservice/controller/PrometheusTestController.java
@@ -10,52 +10,52 @@ import org.springframework.web.bind.annotation.RestController;
 public class PrometheusTestController {
 
     private static int count = 0;
-    @GetMapping("api/ip")
-    public String ipTest(HttpServletRequest request) {
-        String ip = request.getHeader("X_FORWARDED_FOR");
-        return "ip = " + ip;
-    }
-    @GetMapping("api/end-point1")
-    public String endPoint1() {
-
-        return "Metrics for endPoint1";
-    }
-
-    @GetMapping("api/end-point2")
-    public String endpoint2() {
-        return "Metrics for endPoint2";
-    }
-
-    @GetMapping("api/one")
-    public String one() {
-        return "one";
-    }
-
-    @GetMapping("api/two")
-    public String two() {
-        return "two";
-    }
-
-    @GetMapping("api/error1")
-    public String error1() {
-        throw new IllegalStateException("error1");
-    }
-
-    @GetMapping("api/error2")
-    public String error2() {
-        throw new IllegalStateException("error2");
-    }
-
-    @GetMapping("api/error")
-    public int errorRandomTest() {
-        count++;
-        if (count == 3) {
-            count = 0;
-            throw new IllegalStateException("error2");
-        } else {
-            return count;
-        }
-    }
+//    @GetMapping("api/ip")
+//    public String ipTest(HttpServletRequest request) {
+//        String ip = request.getHeader("X_FORWARDED_FOR");
+//        return "ip = " + ip;
+//    }
+//    @GetMapping("api/end-point1")
+//    public String endPoint1() {
+//
+//        return "Metrics for endPoint1";
+//    }
+//
+//    @GetMapping("api/end-point2")
+//    public String endpoint2() {
+//        return "Metrics for endPoint2";
+//    }
+//
+//    @GetMapping("api/one")
+//    public String one() {
+//        return "one";
+//    }
+//
+//    @GetMapping("api/two")
+//    public String two() {
+//        return "two";
+//    }
+//
+//    @GetMapping("api/error1")
+//    public String error1() {
+//        throw new IllegalStateException("error1");
+//    }
+//
+//    @GetMapping("api/error2")
+//    public String error2() {
+//        throw new IllegalStateException("error2");
+//    }
+//
+//    @GetMapping("api/error")
+//    public int errorRandomTest() {
+//        count++;
+//        if (count == 3) {
+//            count = 0;
+//            throw new IllegalStateException("error2");
+//        } else {
+//            return count;
+//        }
+//    }
 
     @GetMapping("/api/info")
     public String info() {
