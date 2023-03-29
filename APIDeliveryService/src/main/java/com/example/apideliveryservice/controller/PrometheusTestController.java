4bbd66da20ed08671bd78a8a18a9c7d13f74fb52
geung <geungan9@gmail.com>
2023-03-30 08:51:43 +0900

    elk test setting

--- APIDeliveryService/src/main/java/com/example/apideliveryservice/controller/PrometheusTestController.java
+++ APIDeliveryService/src/main/java/com/example/apideliveryservice/controller/PrometheusTestController.java
@@ -1,6 +1,5 @@
 package com.example.apideliveryservice.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,54 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class PrometheusTestController {
-
-    private static int count = 0;
-//    @GetMapping("api/ip")
-//    public String ipTest(HttpServletRequest request) {
-//        String ip = request.getHeader("X_FORWARDED_FOR");
-//        return "ip = " + ip;
-//    }
-//    @GetMapping("api/end-point1")
-//    public String endPoint1() {
-//
-//        return "Metrics for endPoint1";
-//    }
-//
-//    @GetMapping("api/end-point2")
-//    public String endpoint2() {
-//        return "Metrics for endPoint2";
-//    }
-//
-//    @GetMapping("api/one")
-//    public String one() {
-//        return "one";
-//    }
-//
-//    @GetMapping("api/two")
-//    public String two() {
-//        return "two";
-//    }
-//
-//    @GetMapping("api/error1")
-//    public String error1() {
-//        throw new IllegalStateException("error1");
-//    }
-//
-//    @GetMapping("api/error2")
-//    public String error2() {
-//        throw new IllegalStateException("error2");
-//    }
-//
-//    @GetMapping("api/error")
-//    public int errorRandomTest() {
-//        count++;
-//        if (count == 3) {
-//            count = 0;
-//            throw new IllegalStateException("error2");
-//        } else {
-//            return count;
-//        }
-//    }
 
     @GetMapping("/api/info")
     public String info() {
@@ -74,5 +25,4 @@ public class PrometheusTestController {
         log.error("error test message");
         return "errorOK";
     }
-
 }
